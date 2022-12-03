4dcfe199a2380b90b945d3cdf5673c2bbc5798e9
skril <skril@DESKTOP-C9875AO.home>
2022-12-03 15:52:17 +0100

    Ejercicios de String Acabados + pagina del ejercicio resuelto

--- src/StringEjs/Ejercicio5.java
+++ src/StringEjs/Ejercicio5.java
@@ -28,7 +28,7 @@ public class Ejercicio5 {
        
        while((pos=texto.indexOf(buscar,pos))>=0){// Lo que hacemos aqui es asignamos a pos a la posicion de la palabra que queremos buscar ejemplo 24, una vez hemos asignado puesto que 24
            pos++;// una vez hemos asignado puesto que 24 es >=0 ahora pos aumenta y es 25, veces ahora vale 1, ahora va a buscar el siguiente texto que queramos buscar y pos vale (por ejemplo) 44
-                // asi hasta que no encuentra el texto que queremos buscar entonces cuando no encuentra indexOf vale -1 y sale del bucle
+                // asi hasta que no encuentra el texto que queremos buscar entonces cuando no encuentra indexOf vale -1 y sale del bucle   EL EJERCICIO ESTÁ AQUI: https://en.calameo.com/read/0063459743e3e5d380626 pagina 90-91
            veces++;// 
        }
        return veces;                                                             
